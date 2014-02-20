40a4f9a353cd800cccb973afd75f1042549f18c9
alexander <alexander@3512612412.(none)>
2014-02-20 18:22:14 -0500

    Postgres initialization files (install script (for ubuntu), schemas etc.)

---
+++ postgres/install.sh
@@ -0,0 +1,11 @@
+#sudo apt-get update
+#sudo apt-get -y install postgresql postgresql-contrib
+
+#Switch to postgres user
+su - postgres
+#Create database
+#createdb bitbox
+#Create tables
+USERNAME=postgres
+FILENAME=templateFile.psql
+psql -U ${USERNAME} -d bitbox -a -f ${FILENAME}
