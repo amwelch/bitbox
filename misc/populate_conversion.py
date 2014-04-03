373f65234765f17c61f1a7210e6de258bb0974f1
Alexander <amwelch@umich.edu>
2014-04-03 20:21:39 +0000

    fetch other currencies

--- misc/populate_conversion.py
+++ misc/populate_conversion.py
@@ -2,6 +2,7 @@ import redis
 import urllib2
 import cookielib
 import sys
+import json
 
 def main(argv):
  
@@ -12,10 +13,25 @@ def main(argv):
     url = "https://blockchain.info/q/24hrprice"
     req= urllib2.Request(url)
     ret = opener.open(req).read()
-    print ret
     value = float(ret)
     con = redis.Redis("localhost")
     con.set("bitbox_btc_to_usd", value);
+
+    url ="http://openexchangerates.org/api/latest.json?app_id=ee635f35bdf94a18933928920ac012ca"
+    req = urllib2.Request(url)
+    ret = json.loads(opener.open(req).read())
+    ret = ret["rates"]
+    con.set("bitbox_usd_to_ca", ret["CAD"])
+    con.set("bitbox_usd_to_eu", ret["EUR"])
+    con.set("bitbox_usd_to_uk", ret["GBP"])
+    con.set("bitbox_usd_to_au", ret["AUD"])
+    con.set("bitbox_usd_to_mx", ret["MXN"])
+    con.set("bitbox_usd_to_br", ret["BRL"])
+    con.set("bitbox_usd_to_ar", ret["ARS"])
+    con.set("bitbox_usd_to_jp", ret["JPY"])
+    con.set("bitbox_usd_to_ch", ret["CNY"])
+
+
 
 if __name__ == '__main__':
     main(sys.argv)
